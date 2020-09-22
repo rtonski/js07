9850f7eaf47a3d4d2df2609c195cbdbd4d4687bc
Rafal Tonski <r.tonski@gmail.com>
2020-09-22 16:48:21 +0200

    js07

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstNumber = 6;
+
+let secondNumber = 9;
+
+function suma(num1, num2) {
+    let wynik = num1 + num2;
+
+    return wynik;
+} 
+
+console.log( suma(firstNumber, secondNumber));
